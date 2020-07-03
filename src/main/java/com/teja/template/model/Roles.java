f8e26f412ad240f4aeb862ab8ebe83487322d0dd
RaviTeja.Poluri <raviteja.poluri1@gmail.com>
2020-07-03 19:30:40 +0530

    Added support for Java12

---
+++ src/main/java/com/teja/template/model/Roles.java
@@ -0,0 +1,5 @@
+package com.teja.template.model;
+
+public enum Roles {
+    ROLE_USER,ROLE_ADMIN
+}
---
+++ src/main/java/com/teja/template/payload/response/MessageResponse.java
@@ -0,0 +1,10 @@
+package com.teja.template.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
